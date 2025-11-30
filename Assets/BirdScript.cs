8367ef3d6b050d954794bffe16fdfa3ad106dfa0
DMargarin <dima.kuzmin.077@mail.ru>
2025-11-30 10:43:32 +0300

    Added UI ver. 0.1, some assets, now you can start game via UI

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour
@@ -9,6 +10,8 @@ public class BirdScript : MonoBehaviour
     public float flapStrength;
     public LogicScript logic;
     public bool birdIsAlive = true;
+    public AudioSource birdFlapSound;
+    private bool isGameOverScreenOn = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,18 +25,37 @@ public class BirdScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive == true)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
+            logic.flapSound();
         }
         
         if (transform.position.y > 14 ||  transform.position.y < -14)
         {
-            logic.gameOver();
-            birdIsAlive = false;
+            birdDeath();
+        }
+
+        if (birdIsAlive == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) == true)
+            {
+                Debug.Log("I'm working");
+                logic.restartGame();
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        logic.gameOver();
-        birdIsAlive = false;
+        birdDeath();
+    }
+
+    public void birdDeath()
+    {
+        if (isGameOverScreenOn == false)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+            isGameOverScreenOn = true;
+        }
+
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -11,17 +11,34 @@ public class LogicScript : MonoBehaviour
     public Text score;
     public Text urScore;
     public Text bestScore;
+    public AudioSource pickCoinSound;
+    public AudioSource pickCoinSoundIfTen;
+    public AudioSource soundIfDeath;
+
+
     private int highScore = 0;
     public GameObject gameOverScreen;
     private bool deleted = false;
+    //private float timer = 0;
+    //public float soundPlays = 0f;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         score.text = playerScore.ToString();
+        
+        if (playerScore % 10 == 0 && playerScore != 0)
+        {
+            pickCoinSoundIfTen.Play();
+        }
+
     }
 
+    public void flapSound()
+    {
+        pickCoinSound.Play();
+    }
     public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -31,11 +48,11 @@ public class LogicScript : MonoBehaviour
     {
         gameOverScreen.SetActive(true);
 
+        soundIfDeath.Play();
+
         urScore.text = "Your score: " + playerScore.ToString();
 
         highScore = PlayerPrefs.GetInt("highScore", 0);
-
-        //Debug.Log(deleted);
 
         if (playerScore > highScore && deleted == false)
         {
@@ -46,11 +63,7 @@ public class LogicScript : MonoBehaviour
         bestScore.text = "Best Score: " + highScore.ToString();
 
         
-
-        if(Input.GetKeyDown(KeyCode.Space) == true)
-        {
-            restartGame();
-        }
+        
     }
 
     public void killBestScore()
