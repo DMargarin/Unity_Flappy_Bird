c7f71b72346a06b3070bdd7aadf1eb7214c5d812
DMargarin <dima.kuzmin.077@mail.ru>
2025-12-03 16:14:42 +0300

    UI ver 0.2, three difficulties for first challenge, Free mode

--- Assets/SceneManagerScript.cs
+++ Assets/SceneManagerScript.cs
@@ -2,14 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagerScript : MonoBehaviour
 {
-   
+
+    public PipeMoveScript pipeMove;
+    
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    
+    public void FreeMode(string sceneName)
+    {
+        pipeMove.boost = 0f;
+        Debug.Log("Easy");
+        LoadScene(sceneName);
+    }
+
+    public void EasyMode(string sceneName)
+    {
+        pipeMove.boost = 0.5f;
+        Debug.Log("Easy");
+        LoadScene(sceneName);
+    }
+
+    public void MediumMode(string sceneName)
+    {
+        pipeMove.boost = 1f;
+        Debug.Log("Medium");
+        LoadScene(sceneName);
+    }
+
+    public void HardMode(string sceneName)
+    {
+        pipeMove.boost = 2f;
+        Debug.Log("Hard");
+        LoadScene(sceneName);
+    }
+
 }
